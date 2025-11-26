be87d1bf8a3609969ecc7413756648cb9149eeec
João Francisco <bunitin@gmail.com>
2025-11-26 15:00:18 -0300

    products complete

--- assets/js/progProdutoAtualizar.js
+++ assets/js/progProdutoAtualizar.js
@@ -13,50 +13,29 @@ btn.addEventListener('click', (e) =>{
     let ativo = document.getElementById('ativo').value
 
 
-    const valores = {
-        nome: nome,
-        descricao: descricao,
-        modelo: modelo,
-        preco: preco,
-        imagem_url: imagem_url,
-        ativo: ativo
-    }
 
+    const valores = {}
+
+    if (nome !== "") valores.nome = nome
+    if (descricao !== "") valores.descricao = descricao
+    if (modelo !== "") valores.modelo = modelo
+    if (preco !== "") valores.preco = preco
+    if (imagem_url !== "") valores.imagem_url = imagem_url
+    
+    // Se ATIVO for nulo no HTMl, converte pra TRUE
+    // Necessário porque como é booleano, não aceita valores != true ou false
+    if(ativo === "null") ativo = true
+
+    if (ativo !== "") valores.ativo = ativo
+
+    const token = sessionStorage.getItem('token')
     console.log(valores)
 
-    // --------------------------------------------- ATUALIZAR PARCIAL --------------------------------------------
-    if(!nome || !descricao || !modelo || !preco || !imagem_url || !ativo){
-
-        console.log('Realizando PATCH')
-        fetch(`http://localhost:3000/produto/:${codProduto}`, {
-            method: 'PATCH',
-            headers: {
-
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },  
-            body: JSON.stringify(valores)
-        })
-        .then(resp => resp.json())
-        .then(dados =>{
-    
-            console.log(dados)
-            res.innerHTML = ''
-            res.innerHTML += dados.message
-            res.style.textAlign = 'center'
-        })
-        .catch((err) =>{
-    
-            console.error('Erro ao atualizar parcialmente: ', err)
-            res.innerHTML = 'Erro ao atualizar parcialmente: ' + err
-            res.style.color = 'red'
-            res.style.textAlign = 'center'
-        }) 
-    // -------------------------------------------- ATUALIZAR COMPLETO --------------------------------------------
-    }else if(!nome || !descricao || !modelo || !preco || !ativo){
+    // --------------------------------------------- ATUALIZAR COMPLETO --------------------------------------------
+    if(nome && descricao && modelo && preco && imagem_url && ativo){
 
         console.log('Realizando PUT')
-        fetch(`http://localhost:3000/produto/:${codProduto}`, {
+        fetch(`http://localhost:3000/produto/${codProduto}`, {
             method: 'PUT',
             headers: {
 
@@ -80,5 +59,33 @@ btn.addEventListener('click', (e) =>{
             res.style.color = 'red'
             res.style.textAlign = 'center'
         })
+    // -------------------------------------------- ATUALIZAR PARCIAL --------------------------------------------
+    }else{
+
+        console.log('Realizando PATCH')
+        fetch(`http://localhost:3000/produto/${codProduto}`, {
+            method: 'PATCH',
+            headers: {
+
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },  
+            body: JSON.stringify(valores)
+        })
+        .then(resp => resp.json())
+        .then(dados =>{
+    
+            console.log(dados)
+            res.innerHTML = ''
+            res.innerHTML += dados.message
+            res.style.textAlign = 'center'
+        })
+        .catch((err) =>{
+    
+            console.error('Erro ao atualizar parcialmente: ', err)
+            res.innerHTML = 'Erro ao atualizar parcialmente: ' + err
+            res.style.color = 'red'
+            res.style.textAlign = 'center'
+        }) 
     }
 })
